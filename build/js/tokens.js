e6355d3fe33b47780077b14caa7f3b97ebafd988
André Silveira <andre.silveira1silva@gmail.com>
2023-10-25 15:48:13 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 25 Oct 2023 01:55:15 GMT
+ */
+
+module.exports = {
+  "VariableCollectionWhite": "#ffffffff",
+  "VariableCollectionPurple500": "#8b5cf6ff",
+  "VariableCollectionGray200": "#c1c1c4ff",
+  "VariableCollectionGray500": "#50505cff",
+  "VariableCollectionGray800": "#1c1c1fff",
+  "VariableCollectionGray900": "#121214ff"
+};
